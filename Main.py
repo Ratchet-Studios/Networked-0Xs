a869b31d0e0607fb6d70ad813a4e5d0dc88cf9f1
Luc Hayward <luchay10@gmail.com>
2018-02-03 17:03:27 +0200

    started board setup

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ isServer = input('Act as server? (Y/N):\n').upper() == "Y"
 board = []
 
 # Setup connections and wait for client to join
-if (isServer):
+if isServer:
     pass
 
 # attempt to join server
@@ -14,9 +14,31 @@ else:
 
 
 # Luc Game
-
+# Board Layout:
+#    0 1 2
+# 0  0|1|2
+# 1  3|4|5
+# 2  6|7|8
 def showBoard():
-    print('\t|\t|\t\n'*3)
+    print('x|o|x')
+    print('o|x|o')
+    print('x|o|x')
 
+def isValidMove(position):
+    return board[position]==0
 
-board = []
+def convertPosition(row,column):
+    return row*3+column
+
+board = [0,0,0,
+         0,0,0,
+         0,0,0]
+
+#show board and ask for a row and a column to play in
+showBoard()
+line = input('Enter a row and column in which to play from 1-3\n Eg: 1,3')
+row = line[0]
+column = line[:-1]
+
+if isValidMove(row,column):
+    pass
