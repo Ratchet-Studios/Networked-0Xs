9c2d47ded9397fad41f98a27c10aa18bc2cc1a5d
Stuart Mesham <stumes3@gmail.com>
2018-02-03 17:08:10 +0200

    Network stuff that doesn't work

--- Main.py
+++ Main.py
@@ -1,18 +1,56 @@
+import socket
+
 # Noughts and cross game where one instance chooses to act as the server and the other as the client.
 
 isServer = input('Act as server? (Y/N):\n').upper() == "Y"
 
-board = []
+if isServer:
+    my_player_number = 1
+    opponent_player_number = 2
+else:
+    my_player_number = 2
+    opponent_player_number = 2
+
+board = [0, 0, 0,
+         0, 0, 0,
+         0, 0, 0]
 
 # Setup connections and wait for client to join
-if (isServer):
-    pass
+if isServer:
+    
+    # create an INET, STREAMing socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind the socket to a public host, and a well-known port
+    server_socket.bind((socket.gethostname(), 4444))
+    # become a server socket
+    server_socket.listen(1)
+    
+    print("Waiting for connection")
+    (client_socket, address) = server_socket.accept()
+    
+    print("Another player connected")
 
 # attempt to join server
 else:
-    pass
+    
+    address = input("Enter server address: ")
+    
+    # create an INET, STREAMing socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # now connect to the web server on port 80 - the normal http port
+    print("Connecting to server")
+    client_socket.connect((address, 4444))
 
+def makeMove(position):
+    board[position] = my_player_number
+    client_socket.send(position)
 
+def receiveMove():
+    receivedMove = client_socket.recv()
+    board[receivedMove] = opponent_player_number
+    
+    
 # Luc Game
 
 def showBoard():
