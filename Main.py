5330b354f91e1840522303b05a207f656857cfc2
Luc Hayward <luchay10@gmail.com>
2018-02-03 16:39:11 +0200

    main setup

---
+++ Main.py
@@ -0,0 +1,12 @@
+# Noughts and cross game where one instance chooses to act as the server and the other as the client.
+
+isServer = input("Act as server? (Y/N):\n")
+
+# Setup connections and wait for client to join
+if (isServer):
+    pass
+
+# attempt to join server
+else:
+    pass
+
